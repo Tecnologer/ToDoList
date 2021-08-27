fd368bad27743c353225531e827a4782de820eae
Rey David <rdominguez@tecnologer.net>
2021-08-26 22:15:52 -0500

    Usuario tests: prueba agregar tarea duplicada y marcar tareas

--- ToDoListSrc/test/UserTests.java
+++ ToDoListSrc/test/UserTests.java
@@ -19,15 +19,34 @@ public class UserTests {
     }
     
     @Test
-    public void addTareasUsersTests() {
+    public void addTareasUsersTests() throws Exception {
         Usuario usuario = new Usuario("oscar");
         Tarea tarea1 = new Tarea("tarea 1");
-        Tarea tarea2 = new Tarea("tarea 2");        
+        Tarea tarea2 = new Tarea("tarea 2");
+        Tarea tarea3 = new Tarea("tarea 1");
         usuario.addTarea(tarea1, tarea2);
         //todo: guardar en db
+        
+        assertThrows(Exception.class, () -> {
+            usuario.addTarea(tarea3);
+        });
+        
         assertEquals(2, usuario.getTareas().size());
         assertNotNull(usuario.getTareaPorDesc("tarea 1"));
         assertNotNull(usuario.getTareaPorDesc("tarea 2"));
+        assertNull(usuario.getTareaPorDesc("tarea 3"));
     }
     
+    @Test
+    public void completarTareasTests() throws Exception {
+        Usuario usuario = new Usuario("oscar");
+        Tarea tarea1 = new Tarea("tarea 1");
+        Tarea tarea2 = new Tarea("tarea 2");
+        usuario.addTarea(tarea1, tarea2);
+        //todo: guardar en db
+        
+        usuario.completarTarea("tarea 1");
+        assertTrue(tarea1.estaCompleta());
+        assertFalse(tarea2.estaCompleta());
+    }
 }
